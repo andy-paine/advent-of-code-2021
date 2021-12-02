1d0184706c7f46b9b462c7a608db2fbc0ef552db
Andy Paine <andrew.paine@sap.com>
2021-12-02 09:21:43 +0000

    Day 2 in Crystal
    Could almost certainly do with some improvement to make it prettier but
    I was trying to do this before standup started so je ne regrette rien

---
+++ day-2/crystal/part-1.cr
@@ -0,0 +1,6 @@
+def amount(vals)
+  vals.map{|v| v.split[1].to_i}.sum
+end
+
+is = STDIN.gets_to_end.strip.split("\n").group_by{|i| i.split[0]}
+puts "#{(amount(is["down"]) - amount(is["up"])) * amount(is["forward"])}"
