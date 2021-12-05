6f5b3ecfd3e802f4c553bb89e6685b58d46b5c1f
Andy Paine <andrew.paine@sap.com>
2021-12-05 11:17:42 +0000

    First pass at day 5 in Crystal

---
+++ day-5/crystal/part-1.cr
@@ -0,0 +1,10 @@
+input = STDIN.gets_to_end.strip.split("\n").map(&.split(" -> ")).map { |s| s.map { |c| Tuple(Int32, Int32).from(c.split(",").map(&.to_i)) } }
+points = input.reduce([] of {Int32, Int32}) do |points, l|
+  s, e = l
+  vec = {e[0]-s[0],e[1]-s[1]}
+  next points unless vec.any?(&.zero?)
+  size = vec.map(&.abs).max
+  nvec = vec.map{|c| (c/size).to_i}
+  points.concat((0...size+1).map{|i| {s[0]+i*nvec[0],s[1]+i*nvec[1]}})
+end
+puts points.tally.select { |_, v| v > 1 }.size
