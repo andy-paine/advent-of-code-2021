8dd49797499456ce39c91144118a09451eda2160
Andy Paine <andy.paine@engineerbetter.com>
2021-12-12 07:49:35 +0000

    Day 11

---
+++ day-11/crystal/part-2.cr
@@ -0,0 +1,15 @@
+def adj(r, c)
+  (-1..1).map { |i| (-1..1).map { |j| {r + i, c + j} } }.flatten.select { |x, y| 0 <= x < 10 && 0 <= y < 10 }
+end
+
+octs = STDIN.gets_to_end.strip.split("\n").map { |l| l.chars.map(&.to_i) }
+step = (1..).find do |i|
+  octs.map!(&.map(&.+ 1))
+  while octs.flatten.any?(&.> 9)
+    flashes = octs.map_with_index { |l, r| l.map_with_index { |v, c| v > 9 ? {r, c} : nil } }.flatten.compact
+    flashes.each { |nr, nc| adj(nr, nc).each { |r, c| octs[r][c] += 1 }; octs[nr][nc] = Int32::MIN }
+  end
+  octs.map! { |l| l.map { |c| c < 0 ? 0 : c } }
+  octs.flatten.all?(&.zero?)
+end
+puts step
